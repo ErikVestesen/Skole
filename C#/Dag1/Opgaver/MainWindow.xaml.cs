8f95002e482116acdc5a75f2be6bc2f50d796698
Erik Vestesen <Erik Vestesen>
2016-08-22 11:52:52 +0200

    Det meste af opgave 1

--- C#/Dag1/Opgaver/MainWindow.xaml.cs
+++ C#/Dag1/Opgaver/MainWindow.xaml.cs
@@ -20,14 +20,46 @@ namespace Opgaver
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
+        int tal = -1;
+        Random r = null;
+        public MainWindow() {
             InitializeComponent();
+            label2.Content = "Insert number";
+            r = new Random();
+            tal = r.Next(1, 101);
+            textBox.Focus();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
+        private void button_Click(object sender, RoutedEventArgs e) {
+            if(textBox.Text != "") { 
+                int a = Convert.ToInt32(textBox.Text);
 
+                if (a > tal) {
+                    label2.Content = "Too high!";
+                } else if (a < tal) {
+                    label2.Content = "Too low!";
+                } else {
+                    label2.Content = "Correct. Congratulation!";
+                }
+            }
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e){
+            
+        }
+
+        private void button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = " ";
+            r = new Random();
+            label2.Content = "Insert a number";
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e){
+            MessageBoxResult mr = MessageBox.Show("Do you want to stop guessing?",
+             "Stopping?", MessageBoxButton.YesNo);
+            if (mr == MessageBoxResult.No)
+                e.Cancel = true;
         }
     }
 }
