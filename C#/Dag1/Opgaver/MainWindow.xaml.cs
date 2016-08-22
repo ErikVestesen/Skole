5685e6148cb47ebdea01244f798829232a730cfa
Erik Vestesen <Erik Vestesen>
2016-08-22 10:43:19 +0200

    opgaver not done

--- C#/Dag1/Opgaver/MainWindow.xaml.cs
+++ C#/Dag1/Opgaver/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Opgaver
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
