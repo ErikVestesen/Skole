848fe405fc5e25be4c1dec9ac3bbacf05b7ac6ba
Erik Vestesen <Craster013014@live.dk>
2016-08-31 11:42:01 +0200

    Opgave 5 done

--- C#/Dag3/Dag3/Opgave5/GrowableArray.cs
+++ C#/Dag3/Dag3/Opgave5/GrowableArray.cs
@@ -8,6 +8,25 @@ namespace Opgave5
 {
     class GrowableArray
     {
-        private int[] array;
+        private int[] array = new int[10];
+        public int this[int i]
+        {
+            get {
+                this.array = growIfNeeded(i);
+                return array[i];
+            }
+
+            set {
+                this.array = growIfNeeded(i);
+                array[i] = value;
+                }
+            }
+
+            private int[] growIfNeeded(int i) {
+                if (i >= array.Length) {
+                    Array.Resize(ref array, i+1);
+                }
+                return array;
+            }
+        }
     }
-}
--- C#/Dag3/Dag3/Opgave5/Program.cs
+++ C#/Dag3/Dag3/Opgave5/Program.cs
@@ -10,6 +10,16 @@ namespace Opgave5
     {
         static void Main(string[] args)
         {
+            GrowableArray g = new GrowableArray();
+            g[0] = 7;
+            g[3] = 9;
+            g[10] = 13;
+            g[25] = 26;
+            Console.WriteLine(g[0]);
+            Console.WriteLine(g[3]);
+            Console.WriteLine(g[10]);
+            Console.WriteLine(g[25]);
+            Console.ReadLine();
         }
     }
 }
