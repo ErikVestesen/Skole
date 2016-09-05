a686696d0cc8c3f2067e4d64cd334eb4d501a078
Erik Vestesen <Erik Vestesen>
2016-09-05 11:56:20 +0200

    update DB-D3-2.2

--- DB/Dag3.sql
+++ DB/Dag3.sql
@@ -9,35 +9,48 @@ create table customer (
 );
 
 insert into customer values(0,'Allan', 100, null, '1990-01-01');
-insert into customer values(1,'Ballan', 110, 'Allan', '1991-01-01');
-insert into customer values(2,'Callan', 120, 'Allan', '1992-01-01');
-insert into customer values(3,'Dallan', 130, 'Callan', '1993-01-01');
-insert into customer values(4,'John Hitler', 140, 'Callan', '1994-01-01');
+insert into customer values(1,'Ballan', 110, 'Allan', '1991-01-5');
+insert into customer values(2,'Callan', 120, 'Allan', '1992-01-17');
+insert into customer values(3,'Dallan', 130, 'Callan', '1993-01-09');
+insert into customer values(4,'John Hitler', 140, 'Callan', '1994-01-28');
 
 -- 2.1B
-delete from customer where created in (select top 20 percent created from customer order by created)
-select * from customer
+--delete from customer where created in (select top 20 percent created from customer order by created)
+--select * from customer
 
--- 2.1C
-Select * from customer
-update customer
-set debt *= 1.1
-output deleted.name, deleted.debt,inserted.name, inserted.debt 
+---- 2.1C
+--Select * from customer
+--update customer
+--set debt *= 1.1
+--output deleted.name, deleted.debt,inserted.name, inserted.debt 
 
 -- 2.1D
+select * from customer
+
   go
-  with customerCTE(guarantor)
+  with customerCTE(name)
   as
   (
-  select name
-  from customer
-  where guarantor = 'Allan'
+	  select name
+	  from customer
+	  where guarantor is null
   union all
-  select c.name
-  from customer as c
-  join customerCTE as cc
-  on c.guarantor = cc.guarantor
+	  select c.name
+	  from customer as c
+	  join customerCTE as cc
+	  on c.guarantor = cc.name
   )
-  select guarantor from customerCTE  
+  select name from customerCTE  
 
  -- 2.2
+-- SELECT k.name 
+-- from customer k 
+-- where Datepart(dd,k.created) = 
+-- (
+--	select Datepart(dd,c.created) as Dag 
+--	FROM customer c) 
+--and 
+--	Datepart(dd,k.created) = 
+--	(
+--	select Datepart(mm,GETDATE()) as idag
+--	)
