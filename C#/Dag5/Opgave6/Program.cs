b32043afe68d9f7ca848f45e946faae5ba74fca6
Erik Vestesen <Erik Vestesen>
2016-09-06 22:15:17 +0200

    something

--- C#/Dag5/Opgave6/Program.cs
+++ C#/Dag5/Opgave6/Program.cs
@@ -10,6 +10,20 @@ namespace Opgave6
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 34, 8, 56, 31, 79, 150, 88, 7, 200, 47, 88, 20 };
+
+            var query = from tal in numbers
+                        where tal.ToString().Length == 2
+                        orderby tal //descending // <-- opgave b
+                        select tal;
+
+            Console.WriteLine();
+            foreach(int k in query) {
+                Console.WriteLine(k);
+            }
+            Console.ReadLine();
+
+
         }
     }
 }
