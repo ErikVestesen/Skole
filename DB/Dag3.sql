f5d6daa7f2e3fd7a6634d49d1e3bdb78a14f9114
Erik Vestesen <Erik Vestesen>
2016-09-08 22:42:57 +0200

    DB-D4-O2.2a done

--- DB/Dag3.sql
+++ DB/Dag3.sql
@@ -10,8 +10,8 @@ create table customer (
 
 insert into customer values(0,'Allan', 100, null, '1990-01-01');
 insert into customer values(1,'Ballan', 110, 'Allan', '1991-01-5');
-insert into customer values(2,'Callan', 120, 'Allan', '1992-01-17');
-insert into customer values(3,'Dallan', 130, 'Callan', '1993-01-09');
+insert into customer values(2,'Callan', 120, 'Allan', '1992-09-08');
+insert into customer values(3,'Dallan', 130, 'Callan', '1993-08-09');
 insert into customer values(4,'John Hitler', 140, 'Callan', '1994-01-28');
 
 -- 2.1B
@@ -43,14 +43,19 @@ select * from customer
   select name from customerCTE  
 
  -- 2.2
--- SELECT k.name 
--- from customer k 
--- where Datepart(dd,k.created) = 
--- (
---	select Datepart(dd,c.created) as Dag 
---	FROM customer c) 
---and 
---	Datepart(dd,k.created) = 
---	(
---	select Datepart(mm,GETDATE()) as idag
---	)
+ SELECT k.name 
+ from customer k 
+ where Datepart(dd,k.created) in 
+ (
+	select Datepart(dd,c.created) as Dag 
+	FROM customer c) 
+and 
+	Datepart(mm,k.created) = 
+	(
+	select Datepart(mm,GETDATE()) as iMM -- Måned
+	)
+and 
+	Datepart(dd,k.created) = 
+	(
+	select Datepart(dd,GETDATE()) as iDD -- Dag
+	)
