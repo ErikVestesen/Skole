f717717ecb1f77ae8c2f745e182b3982d9803a17
Erik Vestesen <Erik Vestesen>
2016-09-13 08:54:19 +0200

    DB-D6-O5.2 Started

---
+++ DB/Dag6.sql
@@ -0,0 +1,13 @@
+
+create view total as
+select p.cpr, navn, po.postdistrikt, firmanavn, po2.postdistrikt as firmapost
+ from person p join postnummer po on po.postnr = p.postnr
+			   join ansati  a on p.cpr = a.cpr
+			   join firma f on a.firmanr = f.firmanr
+			   join postnummer po2 on a.postnr = po2.postnr
+
+Select navn, firmanavn from total
+
+
+-- create clustered index test on person(navn)
+-- select * from person where navn like 'a%' ==> seek, !scan (CTRL + L)
