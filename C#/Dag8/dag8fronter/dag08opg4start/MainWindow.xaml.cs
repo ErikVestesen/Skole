0bd3f79f5dfa395b0182360500c55c80d3eb4a4f
Erik Vestesen <Craster013014@live.dk>
2016-09-26 00:57:41 +0200

    C#-D8 DONE

---
+++ C#/Dag8/dag8fronter/dag08opg4start/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dag8opg4start
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            listBox1.ItemsSource = Service.Instance.Cars;
+            listBox1.ItemTemplateSelector = new CarSelector();
+        }
+    }
+}
