315b7a177da81a86288573e7ee1b6037f27fea2b
Erik Vestesen <Craster013014@live.dk>
2016-09-26 02:09:20 +0200

    C#-D9-O1 Startet, mainWindow scale not working

---
+++ C#/Dag9/dag08opg4start/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dag8opg4start
+{
+  public partial class MainWindow : Window
+  {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            listBox1.ItemsSource = Service.Instance.Cars;
+            listBox1.ItemTemplateSelector = new CarSelector();
+        }
+
+
+        double x = 1;
+        double y = 1;
+        private void btnUp_Click(object sender, RoutedEventArgs e)
+        {
+            x += 0.25;
+            y += 0.25;
+            
+            Transform scale = new ScaleTransform(x, y);
+            //listBox1.RenderTransform = scale;
+            //btnDown.RenderTransform = scale;
+            //btnUp.RenderTransform = scale;
+
+            stPnlMain.RenderTransform = scale;
+            Application.Current.MainWindow.Height *= y;
+            Application.Current.MainWindow.Width *= x;
+
+        }
+
+
+        private void btnDown_Click(object sender, RoutedEventArgs e)
+        {
+            if (x > 0) { 
+                x -= 0.25;
+                y -= 0.25;
+            Transform scale = new ScaleTransform(x, y);
+                //listBox1.RenderTransform = scale;
+                //btnDown.RenderTransform = scale;
+                //btnUp.RenderTransform = scale;
+                //grid1.RenderTransform = scale;
+                stPnlMain.RenderTransform = scale;
+                Application.Current.MainWindow.Height *= y;
+            Application.Current.MainWindow.Width *= x;
+            }
+        }
+    }
+}
---
+++ C#/Dag9/dag08opg4start/MyClasses.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace dag8opg4start
+{
+  public class Car
+  {
+    string name;
+    public string Name
+    {
+      get { return name; }
+      set { name = value; }
+    }
+    int km;
+    public int Km
+    {
+      get { return km; }
+      set { km = value; }
+    }
+
+    int serviceKm;
+    public int ServiceKm
+    {
+      get { return serviceKm; }
+      set { serviceKm = value; }
+    }
+
+    public override string ToString()
+    {
+      return name + " " + km + " " + serviceKm;
+    }
+  }
+
+  public class Service
+  {
+    private static Service service = null;
+
+    List<Car> cars = new List<Car>();
+    public List<Car> Cars
+    {
+      get { return cars; }
+    }
+
+    private Service()
+    {
+      cars.Add(new Car { Name = "BMW 320", Km = 167345, ServiceKm = 165000 });
+      cars.Add(new Car { Name = "Mercedes B", Km = 1645, ServiceKm = 30000 });
+      cars.Add(new Car { Name = "VW Golf", Km = 82843, ServiceKm = 90000 });
+      cars.Add(new Car { Name = "Ford Focus", Km = 53400, ServiceKm = 60000 });
+      cars.Add(new Car { Name = "Opel Corsa", Km = 132582, ServiceKm = 125000 });
+    }
+
+    public static Service Instance
+    {
+      get
+      {
+        if (service == null)
+          service = new Service();
+
+        return service;
+      }
+    }
+  }
+
+  public class CarSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            Car car = (Car)item;
+
+            Window window = Application.Current.MainWindow;
+
+            if (car.ServiceKm > car.Km)
+                return (DataTemplate)window.FindResource("NormalTemplate");
+            return (DataTemplate)window.FindResource("ServiceTemplate");
+
+        }
+    }
+}
+
