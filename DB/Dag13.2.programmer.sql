2ab80984ca2df2a77d276d23952ca882ab16136b
Erik Vestesen <Erik Vestesen>
2016-10-27 21:47:43 +0200

    DB-Dag 13 DONE

--- DB/Dag13.2.programmer.sql
+++ DB/Dag13.2.programmer.sql
@@ -1,3 +1,8 @@
+drop table t1;
+drop table t2;
+drop view t1view;
+
+
 create table t1(
 salary int
 );
@@ -6,4 +11,19 @@ create table t2(
 test varchar(15)
 );
 
-grant  to superuser
+go
+create view t1view 
+as
+SELECT *
+FROM t1
+WHERE salary < 500000
+go
+
+
+GRANT SELECT,INSERT, UPDATE, DELETE ON t1 to superuser
+GRANT SELECT,INSERT, UPDATE, DELETE ON t2 to superuser
+REVOKE DELETE ON t2 to superuser
+
+GRANT SELECT ON t2 to normaluser
+GRANT SELECT ON t1view to normaluser 
+
