e67cbfd257322be50bb1965887e3cbe85e828391
Erik Vestesen <Craster013014@live.dk>
2016-10-31 15:59:09 +0100

    C# update

---
+++ C#/Dag15/Opgave/Models/myClasses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Opgave.Models;
+
+namespace Opgave.Models
+{
+    public class myClasses
+    {
+        [Required(ErrorMessage = "Please enter your age")]
+        public string Age { get; set; }
+    }
+}
---
+++ C#/Dag15/Opgave1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Opgave1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Opgave1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(model guestResponse)
+        {
+            // håndter tilmelding
+
+            return View("Svar", guestResponse);
+        }
+    }
+}
