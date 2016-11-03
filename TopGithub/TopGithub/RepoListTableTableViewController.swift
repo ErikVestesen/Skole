1e14b8261df1f48f630d6f121b1e71bd69149020
DMU MAC 01 <dmumac01@DMU-MAC-01.local>
2016-11-03 12:07:57 +0100

    iOS topGithub Done

--- TopGithub/TopGithub/RepoListTableTableViewController.swift
+++ TopGithub/TopGithub/RepoListTableTableViewController.swift
@@ -30,6 +30,35 @@ class RepoListTableTableViewController: UITableViewController, StateControllerDe
     if let repo = stateController?.repos[indexPath.row] {
       cell.textLabel?.text = repo.name
       cell.detailTextLabel?.text = repo.description
+      if repo.image == nil {
+        //download image
+        //set repo.image = downloadedimage
+        if let imageURL = URL(string: repo.avatarURL) {
+          URLSession.shared.dataTask(with: imageURL, completionHandler: { (data, URLResponse, error) in
+            if error == nil {
+              if let data = data {
+                let image = UIImage(data: data)
+                if let image = image{
+                  //UpdateGUI on main thread
+                  DispatchQueue.main.async {
+                    cell.imageView?.image = image
+                    self.tableview.reloadRows(at: [indexPath], with: UITableViewRowAnimation.fade)
+                  }
+                  
+                  //update the repo in statecontroller
+                  self.stateController?.updateRepoImage(index: indexPath, image: image)
+                  
+                }
+              }
+            }
+          }).resume()
+        }
+        
+        
+      } else {
+        //already have the image
+        cell.imageView?.image = repo.image
+      }
     }
     return cell
   }
